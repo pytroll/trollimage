c9a919a2d4e3ea2d727e976a095998eb5e9ee121
Martin Raspaud <martin.raspaud@smhi.se>
2013-12-02 11:12:03 +0100

    Change development status to beta.
    Signed-off-by: Martin Raspaud <martin.raspaud@smhi.se>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name="trollimage",
       description='Pytroll imaging library',
       author='Martin Raspaud',
       author_email='martin.raspaud@smhi.se',
-      classifiers=["Development Status :: 5 - Production/Stable",
+      classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Science/Research",
                    "License :: OSI Approved :: GNU General Public License v3 " +
                    "or later (GPLv3+)",
