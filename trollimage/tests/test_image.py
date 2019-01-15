bec7e460eb2fcedbc9a02ca2a04f48e651ddc201
Kat Kolman <kkolman@gufi.ssec.wisc.edu>
2019-01-15 12:25:49 -0600

    Add test for badly formatted colormap + minor bugfix

--- trollimage/tests/test_image.py
+++ trollimage/tests/test_image.py
@@ -1045,7 +1045,7 @@ class TestXRImage(unittest.TestCase):
             delay[1].close()
 
         # with colormap provided
-        exp_cmap = {i: (i, 256 - i, i, 255) for i in range(256)}
+        exp_cmap = {i: (i, 255 - i, i, 255) for i in range(256)}
         data = xr.DataArray(da.from_array(np.arange(81).reshape(9, 9, 1), chunks=9),
                             dims=['y', 'x', 'bands'],
                             coords={'bands': ['P']})
@@ -1059,6 +1059,15 @@ class TestXRImage(unittest.TestCase):
             exp = np.arange(81).reshape(9, 9, 1)
             np.testing.assert_allclose(file_data[0], exp[:, :, 0])
             self.assertEqual(cmap, exp_cmap)
+
+        # with bad colormap provided
+        bad_cmap = [[i, [i, i, i]] for i in range(256)]
+        data = xr.DataArray(da.from_array(np.arange(81).reshape(9, 9, 1), chunks=9),
+                            dims=['y', 'x', 'bands'],
+                            coords={'bands': ['P']})
+        img = xrimage.XRImage(data)
+        with NamedTemporaryFile(suffix='.tif') as tmp:
+            self.assertRaises(ValueError, img.save, tmp.name, keep_palette=True, cmap=bad_cmap)
 
         # with input fill value
         data = np.arange(75).reshape(5, 5, 3)
