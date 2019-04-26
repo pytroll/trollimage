pytroll/trollimage

commit 72b835cc1740b7aaa0561ccbbdc008971bbf5736

Implement blend method for XRImage class
This commit implements the blend method for the XRImage class.
This method already exists on the simple Image class. It implements
alpha blending between two XRImage objects.  The XRImage implementation
leaves the original untouched and returns a third, new XRImage object.
The commit also adds documentation and unit tests.

trollimage/xrimage.py
@@ -1011,22 +1011,58 @@ class XRImage(object):
         self.data.data = new_data
         self.data.coords['bands'] = list(mode)
 
-    def blend(self, other):
-        """Alpha blend *other* on top of the current image."""
-        raise NotImplementedError("This method has not be implemented for "
-                                  "xarray support.")
+    def blend(self, src):
+        r"""Alpha blend *src* on top of the current image.
 
-        if self.mode != "RGBA" or other.mode != "RGBA":
-            raise ValueError("Images must be in RGBA")
-        src = other
-        dst = self
-        outa = src.channels[3] + dst.channels[3] * (1 - src.channels[3])
-        for i in range(3):
-            dst.channels[i] = (src.channels[i] * src.channels[3] +
-                               dst.channels[i] * dst.channels[3] *
-                               (1 - src.channels[3])) / outa
-            dst.channels[i][outa == 0] = 0
-        dst.channels[3] = outa
+        Perform `alpha blending`_ of *src* on top of the current image.
+        Alpha blending is defined as:
+
+        .. math::
+
+           \begin{cases}
+            \mathrm{out}_A = \mathrm{src}_A + \mathrm{dst}_A (1 - \mathrm{src}_A) \\
+            \mathrm{out}_{RGB} = \bigl( \mathrm{src}_{RGB} \mathrm{src}_A + \mathrm{dst}_{RGB} \mathrm{dst}_A \left( 1 - \mathrm{src}_A \right) \bigr) \div \mathrm{out}_A \\
+            \mathrm{out}_A = 0 \Rightarrow \mathrm{out}_{RGB} = 0
+           \end{cases}
+
+        Both images must have mode ``"RGBA"``.
+
+        Args:
+            src (:class:`XRImage` with mode ``"RGBA"``)
+                Image to be blended on top of current image.
+
+        .. _alpha blending: https://en.wikipedia.org/w/index.php?title=Alpha_compositing&oldid=891033105#Alpha_blending
+
+        Returns
+            XRImage with mode "RGBA", blended as described above
+
+        """
+        # NB: docstring maths copy-pasta from enwiki
+
+        if self.mode != "RGBA":
+            raise ValueError("Expected self.mode='RGBA', got "
+                f"{self.mode!s}")
+        elif not isinstance(src, XRImage):
+            raise TypeError(f"Expected XRImage, got {type(src)!s}")
+        elif src.mode != "RGBA":
+            raise ValueError("Expected src.mode='RGBA', got "
+                f"{src.mode!s}")
+
+        dstdata = xr.DataArray(
+                np.empty(src.data.shape, dtype="f4"),
+                dims=src.data.dims,
+                coords=src.data.coords)
+        srca = src.data.sel(bands="A")
+        dsta = self.data.sel(bands="A")
+        outa = srca + dsta * (1-srca)
+        for b in "RGB":
+            dstdata.loc[{"bands": b}] = (
+                    (src.data.sel(bands=b) * srca
+                        + self.data.sel(bands=b) * dsta * (1 - srca)
+                    ) / outa)
+            dstdata.loc[{"bands": b}].values[outa.values==0] = 0
+        dstdata.loc[{"bands": "A"}] = outa
+        return self.__class__(dstdata)
 
     def show(self):
         """Display the image on screen."""

trollimage/tests/test_image.py
@@ -1782,7 +1782,45 @@ class TestXRImage(unittest.TestCase):
         pass
 
     def test_blend(self):
-        pass
+        import xarray as xr
+        from trollimage import xrimage
+
+        core1 = np.arange(75).reshape(5, 5, 3) / 75
+        alpha1 = np.linspace(0, 1, 25).reshape(5, 5, 1)
+        arr1 = np.concatenate([core1, alpha1], 2)
+        data1 = xr.DataArray(arr1, dims=['y', 'x', 'bands'],
+                            coords={'bands': ['R', 'G', 'B', 'A']})
+        img1 = xrimage.XRImage(data1)
+
+        core2 = np.arange(75, 0, -1).reshape(5, 5, 3) / 75
+        alpha2 = np.linspace(1, 0, 25).reshape(5, 5, 1)
+        arr2 = np.concatenate([core2, alpha2], 2)
+        data2 = xr.DataArray(arr2, dims=['y', 'x', 'bands'],
+                            coords={'bands': ['R', 'G', 'B', 'A']})
+        img2 = xrimage.XRImage(data2)
+
+        img3 = img1.blend(img2)
+
+        np.testing.assert_allclose(
+                (alpha1 + alpha2 * (1 - alpha1)).squeeze(),
+                img3.data.sel(bands="A"))
+
+        np.testing.assert_allclose(
+            img3.data.sel(bands="R").values,
+            np.array(
+                [[1.        , 0.95833635, 0.9136842 , 0.8666667 , 0.8180645 ],
+                 [0.768815  , 0.72      , 0.6728228 , 0.62857145, 0.5885714 ],
+                 [0.55412847, 0.5264665 , 0.50666666, 0.495612  , 0.49394494],
+                 [0.5020408 , 0.52      , 0.5476586 , 0.5846154 , 0.63027024],
+                 [0.683871  , 0.7445614 , 0.81142855, 0.8835443 , 0.96]]))
+
+        with self.assertRaises(TypeError):
+            img1.blend("Salekhard")
+
+        wrongimg = xrimage.XRImage(
+                xr.DataArray(np.zeros((0, 0)), dims=("y", "x")))
+        with self.assertRaises(ValueError):
+            img1.blend(wrongimg)
 
     def test_replace_luminance(self):
         pass

commit 37105ae2a9b268ca28d0cd22fbdf35ceff269e76

Replace f-strings by Python-2 style formatting
Commit 72b835c broke Python 2 compatibility, but we still need to
support Python 2.  Replace f-strings by the .format method to bring
back Python 2 compatibility.

trollimage/xrimage.py
@@ -1046,12 +1046,14 @@ class XRImage(object):
 
         if self.mode != "RGBA":
             raise ValueError(
-                    f"Expected self.mode='RGBA', got {self.mode!s}")
+                    "Expected self.mode='RGBA', got {md!s}".format(
+                        md=self.mode))
         elif not isinstance(src, XRImage):
-            raise TypeError(f"Expected XRImage, got {type(src)!s}")
+            raise TypeError("Expected XRImage, got {tp!s}".format(
+                tp=type(src)))
         elif src.mode != "RGBA":
-            raise ValueError(
-                    f"Expected src.mode='RGBA', got {src.mode!s}")
+            raise ValueError("Expected src.mode='RGBA', got {sm!s}".format(
+                sm=src.mode))
 
         dstdata = xr.DataArray(
                 np.empty(src.data.shape, dtype="f4"),