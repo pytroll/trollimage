85d9a06d310b5826424ea21d02629943c2e59d9e
David Hoese <david.hoese@ssec.wisc.edu>
2020-05-26 19:53:05 -0500

    Add test for _burn_overlay hack

--- trollimage/tests/test_image.py
+++ trollimage/tests/test_image.py
@@ -2004,3 +2004,22 @@ class TestXRImage(unittest.TestCase):
             self.assertEqual(dummy_args, [({}, ), {}])
             res.data.data.compute()
             self.assertEqual(dummy_args, [(OrderedDict(), 'Hey', 'Jude'), {'chorus': "La lala lalalala"}])
+
+        # Test HACK for _burn_overlay
+        dummy_args = [(OrderedDict(), ), {}]
+
+        def _burn_overlay(pil_obj, *args, **kwargs):
+            dummy_args[0] = args
+            dummy_args[1] = kwargs
+            return pil_obj
+
+        img = xrimage.XRImage(data)
+        with mock.patch.object(xrimage, "PILImage") as pi:
+            pil_img = mock.MagicMock()
+            pi.fromarray = mock.Mock(wraps=lambda *args, **kwargs: pil_img)
+            res = img.apply_pil(_burn_overlay, 'RGB')
+            # check that the pil image generation is delayed
+            pi.fromarray.assert_not_called()
+            # make it happen
+            res.data.data.compute()
+            pil_img.convert.assert_called_with('RGB')
