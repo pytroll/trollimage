698ee6193f01cd13c13420524a93bf4663c3ea18
David Hoese <david.hoese@ssec.wisc.edu>
2023-08-28 13:58:08 -0500

    Refactor colormap sphinx directive

--- doc/trollimage_colormap.py
+++ doc/trollimage_colormap.py
@@ -18,7 +18,7 @@ from trollimage.image import Image
 
 def setup(app):
     """Add custom extension to sphinx."""
-    app.add_directive("trollimage_colormap", SingleColormap)
+    app.add_directive("trollimage_colormap", TrollimageColormapDirective)
 
     return {
         "version": "0.1",
@@ -27,7 +27,7 @@ def setup(app):
     }
 
 
-class SingleColormap(SphinxDirective):
+class TrollimageColormapDirective(SphinxDirective):
     """Custom sphinx directive for generating one or more colormap images."""
 
     required_arguments: int = 1
@@ -38,9 +38,18 @@ class SingleColormap(SphinxDirective):
 
     def run(self) -> list[nodes.Node]:
         """Import and generate colormap images to be inserted into the document."""
-        cmap_name = self.arguments[0]
+        cmap_import_path = self.arguments[0]
         is_category = "category" in self.options
-        cmap_module_name, cmap_var = cmap_name.rsplit(".", 1)
+        colormap_dict = self._get_colormap_dict(cmap_import_path)
+
+        image_nodes = []
+        for cmap_name, cmap_obj in sorted(colormap_dict.items()):
+            image_nodes += self._create_colormap_nodes(cmap_name, cmap_obj, is_category)
+        return image_nodes
+
+    @staticmethod
+    def _get_colormap_dict(cmap_import_path: str) -> dict[str, colormap.Colormap]:
+        cmap_module_name, cmap_var = cmap_import_path.rsplit(".", 1)
         cmap_module = importlib.import_module(cmap_module_name)
         cmap_objects = getattr(cmap_module, cmap_var)
         if not isinstance(cmap_objects, (list, dict)):
@@ -52,16 +61,16 @@ class SingleColormap(SphinxDirective):
                              if cmap_obj is colormap_object][0]
                 cmap_names.append(cmap_name)
             cmap_objects = dict(zip(cmap_names, cmap_objects))
+        return cmap_objects
 
-        image_nodes = []
-        for cmap_name, cmap_obj in sorted(cmap_objects.items()):
-            cb = colormap.colorbar(25, 500, cmap_obj, category=is_category)
-            channels = [cb[i, :, :] for i in range(3)]
-            im = Image(channels=channels, mode="RGB")
-            cmap_fn = os.path.join("_static", "colormaps", f"{cmap_name}.png")
-            im.save(cmap_fn)
+    @staticmethod
+    def _create_colormap_nodes(cmap_name: str, cmap_obj: colormap.Colormap, is_category: bool) -> list[nodes.Node]:
+        cb = colormap.colorbar(25, 500, cmap_obj, category=is_category)
+        channels = [cb[i, :, :] for i in range(3)]
+        im = Image(channels=channels, mode="RGB")
+        cmap_fn = os.path.join("_static", "colormaps", f"{cmap_name}.png")
+        im.save(cmap_fn)
 
-            paragraph = nodes.paragraph(text=cmap_name)
-            image = nodes.image(f".. image:: {cmap_fn}", **{"uri": cmap_fn, "alt": cmap_name})
-            image_nodes += [paragraph, image]
-        return image_nodes
+        paragraph = nodes.paragraph(text=cmap_name)
+        image = nodes.image(f".. image:: {cmap_fn}", **{"uri": cmap_fn, "alt": cmap_name})
+        return [paragraph, image]
