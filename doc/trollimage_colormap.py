d3352e80613e7dc3837578e176d959131c3a975d
David Hoese <david.hoese@ssec.wisc.edu>
2023-08-28 12:14:08 -0500

    Add ability to process multiple colormaps in directive

--- doc/trollimage_colormap.py
+++ doc/trollimage_colormap.py
@@ -39,13 +39,26 @@ class SingleColormap(SphinxDirective):
         cmap_name = self.options["colormap"]
         cmap_module_name, cmap_var = cmap_name.rsplit(".", 1)
         cmap_module = importlib.import_module(cmap_module_name)
-        cmap_obj = getattr(cmap_module, cmap_var)
-        cb = colormap.colorbar(25, 500, cmap_obj)
-        channels = [cb[i, :, :] for i in range(3)]
-        im = Image(channels=channels, mode="RGB")
-        cmap_fn = os.path.join("_static", f"{cmap_var}.png")
-        im.save(cmap_fn)
+        cmap_objects = getattr(cmap_module, cmap_var)
+        if not isinstance(cmap_objects, (list, dict)):
+            cmap_objects = {cmap_var: cmap_objects}
+        if not isinstance(cmap_objects, dict):
+            cmap_names = []
+            for colormap_object in cmap_objects:
+                cmap_name = [cmap_name for cmap_name, cmap_obj in cmap_module.__dict__.items()
+                             if cmap_obj is colormap_object][0]
+                cmap_names.append(cmap_name)
+            cmap_objects = dict(zip(cmap_names, cmap_objects))
 
-        image = nodes.image(f".. image:: {cmap_fn}", **{"uri": cmap_fn, "alt": cmap_var})
-        paragraph = nodes.paragraph(text=cmap_var)
-        return [paragraph, image]
+        image_nodes = []
+        for cmap_name, cmap_obj in sorted(cmap_objects.items()):
+            cb = colormap.colorbar(25, 500, cmap_obj)
+            channels = [cb[i, :, :] for i in range(3)]
+            im = Image(channels=channels, mode="RGB")
+            cmap_fn = os.path.join("_static", "colormaps", f"{cmap_name}.png")
+            im.save(cmap_fn)
+
+            paragraph = nodes.paragraph(text=cmap_name)
+            image = nodes.image(f".. image:: {cmap_fn}", **{"uri": cmap_fn, "alt": cmap_name})
+            image_nodes += [paragraph, image]
+        return image_nodes
