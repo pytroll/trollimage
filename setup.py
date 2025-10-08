0b4f1d30389f09e7e5a5910e1e91efaded81105e
Martin Raspaud <martin.raspaud@smhi.se>
2025-10-08 09:02:32 +0200

    Relicense to Apache v2.0

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Copyright (c) 2013, 2016 Martin Raspaud
-
-# Author(s):
-
-#   Martin Raspaud <martin.raspaud@smhi.se>
-#   Adam.Dybbroe <adam.dybbroe@smhi.se>
-
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Setup for trollimage."""
 import sys
 from typing import Any
@@ -109,7 +86,7 @@ setup(name="trollimage",
           "Topic :: Scientific/Engineering",
           "Programming Language :: Python :: Free Threading :: 1 - Unstable",
       ],
-      license="GPL-3.0-or-later",
+      license="Apache-2.0",
       license_files=["LICENSE.txt", "LICENSE_RIO_COLOR.txt"],
       url="https://github.com/pytroll/trollimage",
       packages=find_packages(),
--- trollimage/tests/utils.py
+++ trollimage/tests/utils.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2021 trollimage developers
-#
-# This file is part of trollimage.
-#
-# trollimage is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# trollimage is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with trollimage.  If not, see <http://www.gnu.org/licenses/>.
 """Helper classes and functions for running and writing tests."""
 
 from contextlib import contextmanager
